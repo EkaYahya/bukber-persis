626ec1ac61c609b9e6035de9018533dd20e9ef6e
Eka Yahya <58022930+EkaYahya@users.noreply.github.com>
2026-03-15 21:41:54 +0700

    Refactor script.js for improved functionality

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // ============================================
 
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwm21JGKGu4mpWF-OlyfJJ4s69M3AJ-FmVRImfA65FJMDgFCNz5k_cFrxAlnZ0HSGgK/exec',
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzzu1FmQPChy4muLzy9skz5C43oNQMlFmgp8mIocUrZhC2vrAn-s2flXr-0BU9UZlyv/exec',
     DRIVE_FOLDER_ID: '1-nMHqGY1e7DgE7tLBrRySPjQL51nHThW',
     EVENT_DATE: '2026-03-18T17:00:00+07:00',
     MAX_FILE_SIZE: 5 * 1024 * 1024
